b0d379a5771b8ff61214cdb4629e83b8f0bab507
yassineAchouyne <ACHOUYNE.YASSINE@ofppt-edu.ma>
2023-03-06 14:51:57 +0100

    add

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+        #container{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 navbar-shrink" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="/">Factur.ma</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="my-2 nav-link" href="/#about">À propos</a></li>
+                    <li class="nav-item"><a class="my-2 nav-link" href="/#services">Services</a></li>
+                    <li class="nav-item"><a class="my-2  nav-link" href="/#contact">Contact</a></li>
+                    <li class="nav-item"><a class="btn btn-primary " href="/register">S'inscrire</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+        <main id="container" class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
